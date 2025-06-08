fd88f264ab2ccf3cc3d04d29bee4ce66841209dd
PabloCarrai <pablohernanjuan@hotmail.com>
2025-06-08 11:31:23 -0300

    va.

---
+++ Inventario.py
@@ -0,0 +1,16 @@
+def menuPrincipal():
+    print("Bienvenido al menu Principal")
+    print("""
+          
+         1) ABM Cliente
+         2) ABM Producto
+         3) ABM Factura
+         4) Salir 
+          
+          
+          
+          """)
+    eleccion=input("Ingrese una opcion")
+
+
+menuPrincipal()    
