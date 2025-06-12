df6e5b0e54f4a7240c9b39efde97e46ce7d724cd
PabloCarrai <pablohernanjuan@hotmail.com>
2025-06-12 17:14:01 -0300

    va

--- Inventario.py
+++ Inventario.py
@@ -1,7 +1,29 @@
+def menuCliente():
+    print("""
+    1) Alta Cliente
+    2) Baja Cliente
+    3) Modificacion Cliente
+    4) Salir      
+    """)
+    eleccion = int(input("Ingrese una opcion"))
+    validarOpcion(eleccion)
+
+
+def moverseMenu(eleccion):
+    if (eleccion == 1):
+        menuCliente()
+    elif (eleccion == 2):
+        pass
+    elif (eleccion == 3):
+        pass
+    elif (eleccion == 4):
+        pass
+
+
 def validarOpcion(eleccion):
     while (eleccion < 1 or eleccion > 4):
         eleccion = int(input("Ingrese una opcion Valida"))
-    print(f"Opcion Valida {eleccion}")
+    moverseMenu(eleccion)
 
 
 def mostrarMenu():
